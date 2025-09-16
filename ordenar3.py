f476ef21e2761f84233f3c2b2210f2811a323837
EmoPapu13 <futemo1307@gmail.com>
2025-09-16 00:02:24 -0600

    primer_intento

--- ordenar3.py
+++ ordenar3.py
@@ -1,15 +1,19 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+El programa lee los tres numeros dados por el usuario y los muestra de mayor a menor
 """
 
 # Declaraciones
-CONSTANTE = valor
+
 
 # Entradas
-entrada = input()
+primer_num = int(input())
+segundo_num = int(input())
+tercer_num = int(input())
 
 # Proceso
-
+num_mayor = max(primer_num,segundo_num,tercer_num)
+num_menor = min(primer_num,segundo_num,tercer_num)
+num_en_medio = primer_num+segundo_num+tercer_num - num_mayor - num_menor
 
 # Salidas
-print(salida)
+print(num_mayor,num_en_medio,num_menor)
